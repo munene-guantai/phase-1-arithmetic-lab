701108a9fc9715cc8c072147b0bd21b16b4b529f
Mikebrian <mikemunene1173@gmail.com>
2023-07-05 17:06:37 +0300

    Complete assignment: Arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 62;
+let num2 = 1;
+let multiply = num1 * num2;
+console.log(multiply)
 
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let num3 = 16;
+let num4 = 12;
+let mod = num3 % num4;
+console.log(mod);
+
+let numberSet = [2, 5, 10, 20, 14];
+let max = Math.max(2, 5, 10,20,14);
+console.log(max);
 // Write your code here
